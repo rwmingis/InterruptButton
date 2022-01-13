13d009f9ccbed23c01cef76a6a42256340f252a3
rwmingis <49131027+rwmingis@users.noreply.github.com>
2022-01-13 22:19:31 +1100

    Update InterruptButtonExample.ino

--- Examples/InterruptButtonExample.ino
+++ Examples/InterruptButtonExample.ino
@@ -46,43 +46,43 @@ void setup() {
   // SETUP THE BUTTONS -----------------------------------------------------------------------------
   // -- Menu/UI Page 0 Functions -------------------
   button1.begin();
-  button1.keyDown[0] =            &menu0Button1keyDown;         // Synchronous, fires as an interrupt so should be IRAM_ATTR and be FAST!
-  button1.keyUp[0] =              &menu0Button1keyUp;           // Synchronous, fires as an interrupt so should be IRAM_ATTR and be FAST!
-  button1.keyPress[0] =           &menu0Button1keyPress;                 
-  button1.longKeyPress[0] =       &menu0Button1longKeyPress; 
-  button1.doubleClick[0] =        &menu0Button1doubleClick; 
-  button1.asyncKeyPress[0] =      [](){ Serial.println("Menu 0, Button 1: Async KeyPress"); };      // Asynchronous, so fires when updated in main loop, can be Lamda
-  button1.asyncLongKeyPress[0] =  [](){ Serial.println("Menu 0, Button 1: Async LongKeyPress"); };  // Asynchronous, so fires when updated in main loop, can be Lamda
-  button1.asyncDoubleClick[0] =   [](){ Serial.println("Menu 0, Button 1: Async DoubleClick"); };   // Asynchronous, so fires when updated in main loop, can be Lamda
+  button1.keyDown[0] =            &menu0Button1keyDown;         // Asynchronous, fires as an interrupt so should be IRAM_ATTR and be FAST!
+  button1.keyUp[0] =              &menu0Button1keyUp;           // Asynchronous, fires as an interrupt so should be IRAM_ATTR and be FAST!
+  button1.keyPress[0] =           &menu0Button1keyPress;        // Asynchronous, fires as an interrupt so should be IRAM_ATTR and be FAST!         
+  button1.longKeyPress[0] =       &menu0Button1longKeyPress; 		// Asynchronous, fires as an interrupt so should be IRAM_ATTR and be FAST!
+  button1.doubleClick[0] =        &menu0Button1doubleClick; 		// Asynchronous, fires as an interrupt so should be IRAM_ATTR and be FAST!
+  button1.syncKeyPress[0] =      [](){ Serial.println("Menu 0, Button 1: Sync KeyPress"); };      // Synchronous, so fires when updated in main loop, can be Lamda
+  button1.syncLongKeyPress[0] =  [](){ Serial.println("Menu 0, Button 1: Sync LongKeyPress"); };  // Synchronous, so fires when updated in main loop, can be Lamda
+  button1.syncDoubleClick[0] =   [](){ Serial.println("Menu 0, Button 1: Sync DoubleClick"); };   // Synchronous, so fires when updated in main loop, can be Lamda
 
   button2.begin();
-  button2.keyDown[0] =            &menu0Button2keyDown;         // Synchronous, fires as an interrupt so should be IRAM_ATTR and be FAST!
-  button2.keyUp[0] =              &menu0Button2keyUp;           // Synchronous, fires as an interrupt so should be IRAM_ATTR and be FAST!
-  button2.keyPress[0] =           &menu0Button2keyPress;                 
-  button2.longKeyPress[0] =       &menu0Button2longKeyPress; 
-  button2.doubleClick[0] =        &menu0Button2doubleClick; 
-  button2.asyncKeyPress[0] =      [](){ Serial.println("Menu 0, Button 2: Async KeyPress"); };      // Asynchronous, so fires when updated in main loop, can be Lamda
-  button2.asyncLongKeyPress[0] =  [](){ Serial.println("Menu 0, Button 2: Async LongKeyPress"); };  // Asynchronous, so fires when updated in main loop, can be Lamda
-  button2.asyncDoubleClick[0] =   [](){ Serial.println("Menu 0, Button 2: Async DoubleClick"); };   // Asynchronous, so fires when updated in main loop, can be Lamda
+  button2.keyDown[0] =            &menu0Button2keyDown;         // Asynchronous, fires as an interrupt so should be IRAM_ATTR and be FAST!
+  button2.keyUp[0] =              &menu0Button2keyUp;           // Asynchronous, fires as an interrupt so should be IRAM_ATTR and be FAST!
+  button2.keyPress[0] =           &menu0Button2keyPress;        // Asynchronous, fires as an interrupt so should be IRAM_ATTR and be FAST!   
+  button2.longKeyPress[0] =       &menu0Button2longKeyPress; 		// Asynchronous, fires as an interrupt so should be IRAM_ATTR and be FAST!
+  button2.doubleClick[0] =        &menu0Button2doubleClick; 		// Asynchronous, fires as an interrupt so should be IRAM_ATTR and be FAST!
+  button2.syncKeyPress[0] =      [](){ Serial.println("Menu 0, Button 2: Sync KeyPress"); };      // Synchronous, so fires when updated in main loop, can be Lamda
+  button2.syncLongKeyPress[0] =  [](){ Serial.println("Menu 0, Button 2: Sync LongKeyPress"); };  // Synchronous, so fires when updated in main loop, can be Lamda
+  button2.syncDoubleClick[0] =   [](){ Serial.println("Menu 0, Button 2: Sync DoubleClick"); };   // Synchronous, so fires when updated in main loop, can be Lamda
 
   // -- Menu/UI Page 1 Functions -------------------
-  button1.keyDown[1] =            &menu1Button1keyDown;         // Synchronous, fires as an interrupt so should be IRAM_ATTR and be FAST!
-  button1.keyUp[1] =              &menu1Button1keyUp;           // Synchronous, fires as an interrupt so should be IRAM_ATTR and be FAST!
-  button1.keyPress[1] =           &menu1Button1keyPress;                 
-  button1.longKeyPress[1] =       &menu1Button1longKeyPress; 
-  button1.doubleClick[1] =        &menu1Button1doubleClick; 
-  button1.asyncKeyPress[1] =      [](){ Serial.println("Menu 1, Button 1: Async KeyPress"); };      // Asynchronous, so fires when updated in main loop, can be Lamda
-  button1.asyncLongKeyPress[1] =  [](){ Serial.println("Menu 1, Button 1: Async LongKeyPress"); };  // Asynchronous, so fires when updated in main loop, can be Lamda
-  button1.asyncDoubleClick[1] =   [](){ Serial.println("Menu 1, Button 1: Async DoubleClick"); };   // Asynchronous, so fires when updated in main loop, can be Lamda
+  button1.keyDown[1] =            &menu1Button1keyDown;         // Asynchronous, fires as an interrupt so should be IRAM_ATTR and be FAST!
+  button1.keyUp[1] =              &menu1Button1keyUp;           // Asynchronous, fires as an interrupt so should be IRAM_ATTR and be FAST!
+  button1.keyPress[1] =           &menu1Button1keyPress;        // Asynchronous, fires as an interrupt so should be IRAM_ATTR and be FAST!         
+  button1.longKeyPress[1] =       &menu1Button1longKeyPress; 		// Asynchronous, fires as an interrupt so should be IRAM_ATTR and be FAST!
+  button1.doubleClick[1] =        &menu1Button1doubleClick; 		// Asynchronous, fires as an interrupt so should be IRAM_ATTR and be FAST!
+  button1.syncKeyPress[1] =      [](){ Serial.println("Menu 1, Button 1: Sync KeyPress"); };      // Synchronous, so fires when updated in main loop, can be Lamda
+  button1.syncLongKeyPress[1] =  [](){ Serial.println("Menu 1, Button 1: Sync LongKeyPress"); };  // Synchronous, so fires when updated in main loop, can be Lamda
+  button1.syncDoubleClick[1] =   [](){ Serial.println("Menu 1, Button 1: Sync DoubleClick"); };   // Synchronous, so fires when updated in main loop, can be Lamda
 
-  button2.keyDown[1] =            &menu1Button2keyDown;         // Synchronous, fires as an interrupt so should be IRAM_ATTR and be FAST!
-  button2.keyUp[1] =              &menu1Button2keyUp;           // Synchronous, fires as an interrupt so should be IRAM_ATTR and be FAST!
-  button2.keyPress[1] =           &menu1Button2keyPress;                 
-  button2.longKeyPress[1] =       &menu1Button2longKeyPress; 
-  button2.doubleClick[1] =        &menu1Button2doubleClick; 
-  button2.asyncKeyPress[1] =      [](){ Serial.println("Menu 1, Button 2: Async KeyPress"); };      // Asynchronous, so fires when updated in main loop, can be Lamda
-  button2.asyncLongKeyPress[1] =  [](){ Serial.println("Menu 1, Button 2: Async LongKeyPress"); };  // Asynchronous, so fires when updated in main loop, can be Lamda
-  button2.asyncDoubleClick[1] =   [](){ Serial.println("Menu 1, Button 2: Async DoubleClick"); };   // Asynchronous, so fires when updated in main loop, can be Lamda
+  button2.keyDown[1] =            &menu1Button2keyDown;         // Asynchronous, fires as an interrupt so should be IRAM_ATTR and be FAST!
+  button2.keyUp[1] =              &menu1Button2keyUp;           // Asynchronous, fires as an interrupt so should be IRAM_ATTR and be FAST!
+  button2.keyPress[1] =           &menu1Button2keyPress;        // Asynchronous, fires as an interrupt so should be IRAM_ATTR and be FAST!        
+  button2.longKeyPress[1] =       &menu1Button2longKeyPress; 		// Asynchronous, fires as an interrupt so should be IRAM_ATTR and be FAST!
+  button2.doubleClick[1] =        &menu1Button2doubleClick; 		// Asynchronous, fires as an interrupt so should be IRAM_ATTR and be FAST!
+  button2.syncKeyPress[1] =      [](){ Serial.println("Menu 1, Button 2: Sync KeyPress"); };      // Synchronous, so fires when updated in main loop, can be Lamda
+  button2.syncLongKeyPress[1] =  [](){ Serial.println("Menu 1, Button 2: Sync LongKeyPress"); };  // Synchronous, so fires when updated in main loop, can be Lamda
+  button2.syncDoubleClick[1] =   [](){ Serial.println("Menu 1, Button 2: Sync DoubleClick"); };   // Synchronous, so fires when updated in main loop, can be Lamda
 
   //Clear any spurious clicks made during setup so we start fresh in the main loop.
   button1.clearInputs(); button2.clearInputs();
@@ -91,43 +91,43 @@ void setup() {
 //== INTERRUPT SERVICE ROUTINES ====================================================================
 //==================================================================================================
 // Button1 ISR's (Top left).
-void IRAM_ATTR menu0Button1keyDown(void)      { Serial.println("Menu 0, Button 1: Sync Key Down"); }      
-void IRAM_ATTR menu0Button1keyUp(void)        { Serial.println("Menu 0, Button 1: Sync Key Up"); }         
-void IRAM_ATTR menu0Button1keyPress(void)     { Serial.println("Menu 0, Button 1: Sync Key Press"); }                 
-void IRAM_ATTR menu0Button1longKeyPress(void) { Serial.println("Menu 0, Button 1: Sync Long Key Press"); } 
+void IRAM_ATTR menu0Button1keyDown(void)      { Serial.println("Menu 0, Button 1: Async Key Down"); }      
+void IRAM_ATTR menu0Button1keyUp(void)        { Serial.println("Menu 0, Button 1: Async Key Up"); }         
+void IRAM_ATTR menu0Button1keyPress(void)     { Serial.println("Menu 0, Button 1: Async Key Press"); }                 
+void IRAM_ATTR menu0Button1longKeyPress(void) { Serial.println("Menu 0, Button 1: Async Long Key Press"); } 
 void IRAM_ATTR menu0Button1doubleClick(void)  {
-  Serial.print("Menu 0, Button 1: Sync Double Click - Changing to Menu Level ");
+  Serial.print("Menu 0, Button 1: Async Double Click - Changing to Menu Level ");
   InterruptButton::setMenuLevel(1);
   Serial.println(InterruptButton::getMenuLevel());
 } 
 
-void IRAM_ATTR menu1Button1keyDown(void)      { Serial.println("Menu 1, Button 1: Sync Key Down"); }      
-void IRAM_ATTR menu1Button1keyUp(void)        { Serial.println("Menu 1, Button 1: Sync Key Up"); }         
-void IRAM_ATTR menu1Button1keyPress(void)     { Serial.println("Menu 1, Button 1: Sync Key Press"); }                 
-void IRAM_ATTR menu1Button1longKeyPress(void) { Serial.println("Menu 1, Button 1: Sync Long Key Press"); } 
+void IRAM_ATTR menu1Button1keyDown(void)      { Serial.println("Menu 1, Button 1: Async Key Down"); }      
+void IRAM_ATTR menu1Button1keyUp(void)        { Serial.println("Menu 1, Button 1: Async Key Up"); }         
+void IRAM_ATTR menu1Button1keyPress(void)     { Serial.println("Menu 1, Button 1: Async Key Press"); }                 
+void IRAM_ATTR menu1Button1longKeyPress(void) { Serial.println("Menu 1, Button 1: Async Long Key Press"); } 
 void IRAM_ATTR menu1Button1doubleClick(void)  { 
-  Serial.print("Menu 1, Button 1: Sync Double Click - Changing Back to Menu Level ");
+  Serial.print("Menu 1, Button 1: Async Double Click - Changing Back to Menu Level ");
   InterruptButton::setMenuLevel(0);
   Serial.println(InterruptButton::getMenuLevel());
 } 
 
 // Button2 ISR's (Bottom Left).
-void IRAM_ATTR menu0Button2keyDown(void)      { Serial.println("Menu 0, Button 2: Sync Key Down"); }      
-void IRAM_ATTR menu0Button2keyUp(void)        { Serial.println("Menu 0, Button 2: Sync Key Up"); }         
-void IRAM_ATTR menu0Button2keyPress(void)     { Serial.println("Menu 0, Button 2: Sync Key Press"); }                 
-void IRAM_ATTR menu0Button2longKeyPress(void) { Serial.println("Menu 0, Button 2: Sync Long Key Press"); } 
+void IRAM_ATTR menu0Button2keyDown(void)      { Serial.println("Menu 0, Button 2: Async Key Down"); }      
+void IRAM_ATTR menu0Button2keyUp(void)        { Serial.println("Menu 0, Button 2: Async Key Up"); }         
+void IRAM_ATTR menu0Button2keyPress(void)     { Serial.println("Menu 0, Button 2: Async Key Press"); }                 
+void IRAM_ATTR menu0Button2longKeyPress(void) { Serial.println("Menu 0, Button 2: Async Long Key Press"); } 
 void IRAM_ATTR menu0Button2doubleClick(void)  { 
-  Serial.print("Menu 0, Button 2: Sync Double Click - Changing to Menu Level ");
+  Serial.print("Menu 0, Button 2: Async Double Click - Changing to Menu Level ");
   InterruptButton::setMenuLevel(1);
   Serial.println(InterruptButton::getMenuLevel());
 } 
 
-void IRAM_ATTR menu1Button2keyDown(void)      { Serial.println("Menu 1, Button 2: Sync Key Down"); }      
-void IRAM_ATTR menu1Button2keyUp(void)        { Serial.println("Menu 1, Button 2: Sync Key Up"); }         
-void IRAM_ATTR menu1Button2keyPress(void)     { Serial.println("Menu 1, Button 2: Sync Key Press"); }                 
-void IRAM_ATTR menu1Button2longKeyPress(void) { Serial.println("Menu 1, Button 2: Sync Long Key Press"); } 
+void IRAM_ATTR menu1Button2keyDown(void)      { Serial.println("Menu 1, Button 2: Async Key Down"); }      
+void IRAM_ATTR menu1Button2keyUp(void)        { Serial.println("Menu 1, Button 2: Async Key Up"); }         
+void IRAM_ATTR menu1Button2keyPress(void)     { Serial.println("Menu 1, Button 2: Async Key Press"); }                 
+void IRAM_ATTR menu1Button2longKeyPress(void) { Serial.println("Menu 1, Button 2: Async Long Key Press"); } 
 void IRAM_ATTR menu1Button2doubleClick(void)  { 
-  Serial.print("Menu 1, Button 2: Sync Double Click - Changing Back to Menu Level ");
+  Serial.print("Menu 1, Button 2: Async Double Click - Changing Back to Menu Level ");
   InterruptButton::setMenuLevel(0);
   Serial.println(InterruptButton::getMenuLevel());
 } 
@@ -136,8 +136,8 @@ void IRAM_ATTR menu1Button2doubleClick(void)  {
 //===========================================================================================================
 
 void loop() { 
-  // ACTION ANY ASYNCHRONOUS BUTTON EVENTS NOT DONE SYNCHRONOUSLY IN ISR
-  button1.processAsyncEvents(); button2.processAsyncEvents();
+  // ACTION ANY SYNCHRONOUS BUTTON EVENTS NOT DONE ASYNCHRONOUSLY IN ISR
+  button1.processSyncEvents(); button2.processSyncEvents();
 
   delay(1000);
 }
