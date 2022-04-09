6bcd5ebe187c0eb6d6a0186142f6509abc045a2a
rwmingis <49131027+rwmingis@users.noreply.github.com>
2022-04-09 17:05:18 +1000

    Update InterruptButton.h

--- InterruptButton.h
+++ InterruptButton.h
@@ -13,15 +13,15 @@
 #include "freertos/task.h"
 #include "freertos/queue.h"
 
-#define ASYNC_EVENT_QUEUE_DEPTH   5     // This queue is service very quickly so can be short
+#define ASYNC_EVENT_QUEUE_DEPTH   5     // This queue is serviced very quickly so can be short
 #define SYNC_EVENT_QUEUE_DEPTH    10    // This queue is limited to mainloop frequency so actions can backup
 
-typedef void (*func_ptr_t)(void);       // Type def to faciliate manage pointers to external action functions
+typedef void (*func_ptr_t)(void);       // Typedef to faciliate managing pointers to external action functions
 
 enum modes {
   Mode_Asynchronous,                    // All actions performed via Asynchronous RTOS queue
   Mode_Hybrid,                          // keyUp and keyDown performed by RTOS queue, remaining actions by Static Synchronous Queue.
-  Mode_Synchronous                      // All actions performed by Static Synchronous Queue.
+  Mode_Synchronous                      // All actions performed by Synchronous Queue (static class member array, FIFO).
 };
 
 enum events:uint8_t {
@@ -32,7 +32,7 @@ enum events:uint8_t {
   Event_AutoRepeatPress,
   Event_DoubleClick,
   NumEventTypes,                        // Not an event, but this value used to size the number of columns in event/action array.
-  Event_All
+  Event_All                             // Used to enable or disable all events
 };
 
 
@@ -49,9 +49,15 @@ class InterruptButton {
       Releasing
     };
 
+    struct argPasser {
+      InterruptButton *btn;
+      gpio_num_t      pinNumber;
+    } ;
+
     // STATIC class members shared by all instances of this object (common across all instances of the class)
     // ------------------------------------------------------------------------------------------------------
-    static void queueServicer(void* pvParams);                              // Function used as RTOS task to receive and process action from RTOS message queue.
+    static void asyncQueueServicer(void* pvParams);                         // Function used as RTOS task to receive and process action from RTOS message queue.
+    static bool buttonRegistered(gpio_num_t pinNumber);                     // Used to determine if a button is a registered (valid) button
     static void readButton(void* arg);                                      // function to read button state (must be static to bind to GPIO and timer ISR)
     static void longPressEvent(void *arg);                                  // Callback to excecute a longPress event, called by timer
     static void autoRepeatPressEvent(void *arg);                            // Callback to excecute a autoRepeatPress event, called by timer
@@ -62,30 +68,47 @@ class InterruptButton {
                            InterruptButton* btn, 
                            const char *msg); 
     static void killTimer(esp_timer_handle_t &timer);                       // Helper function to kill a timer
+    
+    // Action fuction is a helper function used to simplify queueing tasks either from ISR or timer
+    static void action(InterruptButton  *btn,                               
+                       events           event,                                
+                       uint8_t          menuLevel, 
+                       BaseType_t       *pxHigherPriorityTaskWoken);        // If this argument is supplied, then it is assumed to be called from an ISR
+
+    static void action(InterruptButton  *btn,                               // Helper function to simplify calling actions at specified menulevel
+                       events           event,                                
+                       uint8_t          menuLevel);
+
+    // These inline functions allow user to default to current menu level if not specified
+    inline static void action(InterruptButton* btn, events event, BaseType_t *pxHigherPriorityTaskWoken) { action(btn, event, m_menuLevel, pxHigherPriorityTaskWoken); };
+    inline static void action(InterruptButton* btn, events event) { action(btn, event, m_menuLevel); };
+
+
+    
 
     static const uint8_t  m_targetPolls = 10;                               // Desired number of polls required to debounce a button
     static bool           m_classInitialised;                               // Boolean flag to control class initialisation
-    static QueueHandle_t  asyncEventQueue;                                  // Pointer/handle to the RTOS message queue handling actions.
-    static TaskHandle_t   queueServicerHandle;                              // Pointer/handle to the RTOS task that actions the RTOS Queue messages
-    static func_ptr_t     syncEventQueue[];                                 // Array used as the Static Synchronous Event Queue
+    static TaskHandle_t   m_asyncQueueServicerHandle;                       // Pointer/handle to the RTOS task that actions the RTOS Queue messages
+    static func_ptr_t     m_asyncEventQueue[];                               // Array used as the Static Synchronous Event Queue
+    static func_ptr_t     m_syncEventQueue[];                               // Array used as the Static Synchronous Event Queue
     static bool           m_firstButtonInitialised;                         // Used to block any further changes to m_numMenus
     static uint8_t        m_numMenus;                                       // Total number of menu sets, can be set by user, but only before initialising first button
     static uint8_t        m_menuLevel;                                      // Current menulevel for all buttons (global in class so common across all buttons)
     static modes          m_mode;
+    static uint64_t       m_btnRegister;                                    // Used to determine if button valid (in case it was deleted) before processing it asychrnously
 
     // Non-static instance specific member declarations
     // ------------------------------------------------
     void                  initialiseInstance(void);                         // Setup interrupts and event-action array
-
     bool                  m_thisButtonInitialised = false;                  // Allows us to intialise when binding functions (ie detect if already done)
     gpio_num_t            m_pin;                                            // Button gpio
     uint8_t               m_pressedState;                                   // State of button when it is pressed (LOW or HIGH)
     gpio_mode_t           m_pinMode;                                        // GPIO mode: IDF's input/output mode
     volatile buttonStates m_state;                                          // Instance specific state machine variable (intialised when intialising button)
     volatile bool         m_wtgForDblClick = false;
-    esp_timer_handle_t    m_buttonPollTimer;                                // Instance specific timer for button debouncing
-    esp_timer_handle_t    m_buttonLPandRepeatTimer;                         // Instance specific timer for button longPress and autoRepeat timing
-    esp_timer_handle_t    m_buttonDoubleClickTimer;                         // Instance specific timer for discerning double-clicks from regular keyPresses
+    esp_timer_handle_t    m_buttonPollTimer = nullptr;                      // Instance specific timer for button debouncing
+    esp_timer_handle_t    m_buttonLPandRepeatTimer = nullptr;               // Instance specific timer for button longPress and autoRepeat timing
+    esp_timer_handle_t    m_buttonDoubleClickTimer = nullptr;               // Instance specific timer for discerning double-clicks from regular keyPresses
 
     volatile uint8_t      m_doubleClickMenuLevel;                           // Stores current menulevel while differentiating between regular keyPress or a double-click
     uint16_t              m_pollIntervalUS;                                 // Timing variables
@@ -98,7 +121,7 @@ class InterruptButton {
     volatile uint16_t     m_totalPolls = 0;
 
     func_ptr_t**          eventActions = nullptr;                           // Pointer to 2D array, event actions by row, menu levels by column.
-    uint16_t              eventMask = 0b0000010000111;                      // Default to keyUp, keyDown, and keyPress enabled, and no blanked disable
+    uint16_t              eventMask = 0b0000010000111;                      // Default to keyUp, keyDown, and keyPress enabled, and no blanket disable
                                                                             // When binding functions, longKeyPress, autoKeyPresses, & double-clicks are automatically enabled.
 
   public:
@@ -142,11 +165,6 @@ class InterruptButton {
     void            unbind(events   event,                                  // Used to unbind an action to an event at a given menulevel
                            uint8_t  menuLevel);
     inline void     unbind(events event) { unbind(event, m_menuLevel); };   // Above function defaulting to current menulevel
-
-    void            action(events     event,                                // Helper function to simplify calling actions at specified menulevel
-                           uint8_t    menuLevel, 
-                           BaseType_t *pxHigherPriorityTaskWoken = nullptr); 
-    inline void     action(events event, BaseType_t *pxHigherPriorityTaskWoken = nullptr) { action(event, m_menuLevel, pxHigherPriorityTaskWoken); };
 };
 
 #endif
